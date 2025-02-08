40fd15a359ba97c51bafc7124a1adfab3491a888
giorgi <george.kvrivi@gmail.com>
2025-02-08 12:52:42 +0400

    feat: add response headers and wrap up auth implementation

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, resources={r"/*": {"origins": [os.getenv('CLIENT_URL')]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": [os.getenv('CLIENT_URL')]}})
 
 if __name__ == "__main__":  
       app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,9 @@ main = Blueprint("main", __name__)
 @main.after_request
 def add_headers(response):
     response.headers['Access-Control-Allow-Origin'] = os.getenv("CLIENT_URL")
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Credentials'] = 'true',
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
 @main.route("/")
