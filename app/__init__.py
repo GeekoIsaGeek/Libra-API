a67bb8f35c6d7bde9f083623e649c8e1f6063a09
geekoisageek <george.kvrivi@gmail.com>
2025-02-13 09:07:46 +0400

    init: configure db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from app.extensions import jwt
 from app.helpers.book_repository import init_books
 from app.routes.main import main
-import os
+from app.models import db
+from flask_migrate import Migrate
 
 def create_app():
    app = Flask(__name__)
@@ -13,10 +15,15 @@ def create_app():
 
    app.register_blueprint(main)
 
+   db.init_app(app)
+   migrate = Migrate(app, db)
+
    with app.app_context():
       os.mkdir(app.config['IMAGE_FOLDER']) if not os.path.exists(app.config['IMAGE_FOLDER']) else None
       os.mkdir(app.config['BOOK_FOLDER']) if not os.path.exists(app.config['BOOK_FOLDER']) else None
+
+      db.create_all()
       
-      init_books()
+      # init_books()
       
    return app
--- app/config.py
+++ app/config.py
@@ -1,17 +1,19 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-   ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp", "pdf", 'epub'}
-
    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+   DB_PATH = os.path.join(os.path.abspath(os.path.dirname(__name__)), "database.db")
 
    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
    IMAGE_FOLDER = os.path.join(UPLOAD_FOLDER, "images")
    BOOK_FOLDER = os.path.join(UPLOAD_FOLDER, "books")
+
+   SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
+   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
