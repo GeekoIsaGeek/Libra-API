f7d6cd6463b7a9e83076af4c2b55de251843ea3f
geekoisageek <george.kvrivi@gmail.com>
2025-02-13 22:54:57 +0400

    feat: add user and book models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
 
 db = SQLAlchemy()
+
+class User(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(50), unique=True, nullable=False)
+   email = db.Column(db.String(120), unique=True, nullable=False)
+   password = db.Column(db.String(120), nullable=False)
+   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+   updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
+
+   def to_dict(self):
+       return {
+           'id': self.id,
+           'username': self.username,
+           'email': self.email,
+           'created_at': self.created_at,
+           'updated_at': self.updated_at
+       }
+
+class Book(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   title = db.Column(db.JSON, nullable=False)
+   author = db.Column(db.JSON, nullable=False)
+   description = db.Column(db.JSON, nullable=False)
+   language = db.Column(db.JSON, nullable=False)
+   year = db.Column(db.Integer, nullable=False)
+   pages = db.Column(db.Integer, nullable=False)
+   tags = db.Column(db.JSON, nullable=False)
+   image = db.Column(db.String(120), nullable=False)
+   slug = db.Column(db.String(120), nullable=False)
+   file = db.Column(db.String(120), nullable=False)
+   created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+   created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+   updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
+
+   def to_dict(self):
+       return {
+           'id': self.id,
+           'title': json.loads(self.title),
+           'author': json.loads(self.author),
+           'description': json.loads(self.description),
+           'language': json.loads(self.language),
+           'year': self.year,
+           'pages': self.pages,
+           'tags': json.loads(self.tags),
+           'image': self.image,
+           'slug': self.slug,
+           'file': self.file,
+           'created_by': self.created_by,
+       }
